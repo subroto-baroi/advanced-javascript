f044a8007c34304d7c6e8750b1f84dbec8041e84
subroto-baroi <67515709+subroto-baroi@users.noreply.github.com>
2020-08-07 20:58:43 +0600

    add save change

--- null-vs-undefined.js
+++ null-vs-undefined.js
@@ -1,25 +1,25 @@
-// let name;             // value is not set so undefined
-// console.log(name); 
+let name;             // value is not set so undefined
+console.log(name); 
 
 
-// function add (num1, num2){
-//     const result = num1 + num2;   // result is not return so undefined
-// }
-// const result = add(20,42);
-// console.log(result);
+function add (num1, num2){
+    const result = num1 + num2;   // result is not return so undefined
+}
+const result = add(20,42);
+console.log(result);
 
 
-// function add (num1, num2){
-//     const result = num1 + num2;    
-//     return result;
-// }   
-// const result = add(20);          // second parameter value is not declare so undefined
-// console.log(result);
+function add (num1, num2){
+    const result = num1 + num2;    
+    return result;
+}   
+const result = add(20);          // second parameter value is not declare so undefined
+console.log(result);
 
 
-// const friends ={name:'rani', age:17, phone:'01738', address:'gazipur'};
-// console.log(friends.school);    // school property is not found in object so undefined
+const friends ={name:'rani', age:17, phone:'01738', address:'gazipur'};
+console.log(friends.school);    // school property is not found in object so undefined
 
 
-// let person = undefined;     // set value is undefined so undefined
-// console.log(person);
+let person = undefined;     // set value is undefined so undefined
+console.log(person);
