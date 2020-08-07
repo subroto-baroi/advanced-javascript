24cb4ffe26d5100d95a85494c8be1d1ce5fb62ff
subroto-baroi <67515709+subroto-baroi@users.noreply.github.com>
2020-08-07 18:06:07 +0600

    add change file

---
+++ true-false.js
@@ -0,0 +1,74 @@
+
+// for number
+const age = -15;
+
+if (age){
+    console.log('condition is true');
+}
+else {
+    console.log('condition is false');
+}
+
+// for string
+
+const name = {};
+
+if (name){
+    console.log('condition is true');
+}
+else {
+    console.log('condition is false');
+}
+
+
+// undefined value 
+
+const name;
+
+if (name){
+    console.log('condition is true');
+}
+else {
+    console.log('condition is false');
+}
+
+// when 0 is true declare
+
+const age = 0;
+
+if (name || name == 0){
+    console.log('condition is true');
+}
+else {
+    console.log('condition is false');
+}
+
+// False list bellow
+
+// ""
+
+// undefined
+
+// 0
+
+// false
+
+// NaN
+
+// Null
+ 
+
+// True list is bellow
+
+// "0"
+
+// "false"
+
+// []
+
+// {}
+
+// " "
+
+
+
