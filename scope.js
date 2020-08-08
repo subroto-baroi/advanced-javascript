2df045eac602b6094828398e393cde0570c7198a
subroto-baroi <67515709+subroto-baroi@users.noreply.github.com>
2020-08-08 13:17:47 +0600

    add scop access

---
+++ scope.js
@@ -0,0 +1,36 @@
+// global and local scope
+
+// let num = 30;   // global scope call to any where in the page.
+// function sum(first, second) {
+//     let result =first + second + num; // local scope call to only in the function.
+//     console.log(result);
+//     console.log(num);
+
+//     return result;
+    
+// }
+// const output =sum(20,30);
+// console.log(num)
+// console.log(output);
+
+
+// another example
+
+let num = 30;   
+function sum(first, second) {
+    let result =first + second;
+    if(result > 9){
+        
+        const mind ='happy';
+            mind ='unhappy';
+            mind ='good';
+        var mind ='so good';
+        console.log(mind);  // const access in the scope
+
+    }
+            
+
+}console.log(mind); // var access out side from scope
+
+    
+
